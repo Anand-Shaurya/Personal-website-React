a0fa45f7988941099c68ed6490a058298d741a7c
Anand Shaurya <shaurya252anand@gmail.com>
2024-08-02 23:29:24 +0530

    Added hobbies

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Certifications = () => {
   return (
     <section id="certifications">
-      <h2>Certifications & Achievements</h2>
+      <h2>Certifications & Achievements & Hobbies</h2>
       <ul>
         <li>National Talent Search Examination (NTSE) scholar</li>
         <li>Cleared JEE Advanced 2020 with All India Rank 2726</li>
         <li>Taught and donated educational materials to underprivileged children in association with Kartavya</li>
+        <li>Loves to read books, and do paper crafts in free time</li>
       </ul>
     </section>
   );
